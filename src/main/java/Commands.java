60cccec5fa6015cb276cd6d07596088766516e64
seanlaiys <seanlaiyuensheng@gmail.com>
2022-01-20 17:53:48 +0800

    Created list of inputable commands.

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Commands {
+/**
+ *
+ * This class contains commands the user could input to invoke methods
+ * to their command
+ *
+ */
+public class Commands {
+    public static final String COMMAND_BYE = "bye";
+    public static final String COMMAND_LIST = "list";
+    public static final String COMMAND_BLAH = "blah";
 }
