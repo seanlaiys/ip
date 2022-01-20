81da6ee733a965809a9a66e610c3700e7f6f9dd9
seanlaiys <seanlaiyuensheng@gmail.com>
2022-01-20 17:50:21 +0800

    Created Chatbot Alfred

---
+++ src/main/java/Alfred.java
@@ -0,0 +1,11 @@
+public class Duke {
+    public static void main(String[] args) {
+        String logo =
+                  "    __      \n"
+                + "   /  \\    \n"
+                + "  / /\\ \\  \n"
+                + " / /__\\ \\  \n"
+                + "/_/    \\_\\ \n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
---
+++ src/main/java/Messages.java
@@ -0,0 +1,34 @@
+/**
+ *
+ * This class contains methods which prints messages Alfred could send
+ * to the users according to what the users input
+ *
+ */
+public class Texts {
+    private static final String logo =
+            "    __      \n"
+                    + "   /  \\    \n"
+                    + "  / /\\ \\  \n"
+                    + " / /__\\ \\  \n"
+                    + "/_/    \\_\\ \n";
+
+    private static final String greetings =
+            "Greetings, my name is Alfred. \n"
+                    + "How may I assist you?";
+
+    private static final String list =
+            "Here is your list.";
+
+    private static final String blah =
+            "Blah.";
+
+    private static final String bye =
+            "It has been pleasant to be at your service. Goodbye.";
+
+    public static void greetUser() {
+        System.out.println(logo);
+        System.out.println(greetings);
+    }
+
+    public static void
+}
