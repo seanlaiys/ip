5328cb3129656f4ad2cf953672bff17227c0f0ec
seanlaiys <seanlaiyuensheng@gmail.com>
2022-01-20 17:53:27 +0800

    Created class to read commands bye, list and blah.

--- src/main/java/CommandReader.java
+++ src/main/java/CommandReader.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class CommandReader {
+import java.util.Scanner;
+/**
+ *
+ * This class serves to take in the input keyed in by the user
+ * and invokes methods in response to what the user has keyed in.
+ *
+ */
+public class CommandReader {
+    public static void readCommand() {
+        String input;
+        Scanner user_input = new Scanner(System.in);
+        input = user_input.nextLine();
+        while(!input.equals(Commands.COMMAND_BYE)) {
+            if(input.contains(Commands.COMMAND_LIST)) {
+                input = Messages.generateList(user_input);
+            }
+            else if (input.contains(Commands.COMMAND_BLAH)) {
+                input = Messages.sayBlah(user_input);
+            }
+            else {
+                input = user_input.nextLine();
+            }
+        }
+        Messages.sayGoodbye();
+    }
 }
