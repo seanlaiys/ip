2d0abafba482dfaf6673c7a2782d067a81c4450a
seanlaiys <seanlaiyuensheng@gmail.com>
2022-01-20 17:54:21 +0800

    Created methods that print messages depending on user input.

--- src/main/java/Messages.java
+++ src/main/java/Messages.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 /**
  *
  * This class contains methods which prints messages Alfred could send
  * to the users according to what the users input
  *
  */
-public class Texts {
+public class Messages {
     private static final String logo =
             "    __      \n"
                     + "   /  \\    \n"
@@ -16,19 +18,30 @@ public class Texts {
             "Greetings, my name is Alfred. \n"
                     + "How may I assist you?";
 
-    private static final String list =
-            "Here is your list.";
+    private static final String list = "Here is your list.";
 
-    private static final String blah =
-            "Blah.";
+    private static final String blah = "Blah.";
 
-    private static final String bye =
-            "It has been pleasant to be at your service. Goodbye.";
+    private static final String bye = "It has been pleasant to be at your service. Goodbye.";
 
     public static void greetUser() {
         System.out.println(logo);
         System.out.println(greetings);
     }
 
-    public static void
+    public static String generateList(Scanner user_input) {
+        System.out.println(list);
+        String input = user_input.nextLine();
+        return input;
+    }
+
+    public static String sayBlah(Scanner user_input) {
+        System.out.println(blah);
+        String input = user_input.nextLine();
+        return input;
+    }
+
+    public static void sayGoodbye() {
+        System.out.println(bye);
+    }
 }
