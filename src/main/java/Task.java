d77d93a3736a40c1b47370df2298cf430e18c7ad
seanlaiys <seanlaiyuensheng@gmail.com>
2022-01-28 00:04:12 +0800

    Add abstract method getDate()

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 /**
  *
  * This class encapsulates a Task and its status.
@@ -9,17 +11,27 @@ public abstract class Task {
     protected static final String DONE = "[X]";
     protected static final String NOTDONE = "[ ]";
 
+    /**
+     * Creates a new Task object
+     */
     public Task(String task) {
         this.task = task;
         this.isDone = false;
     }
 
+    /**
+     * Marks a Task object done
+     */
     public void mark() {
         this.isDone = true;
     }
 
+    /**
+     * Marks a Task object undone
+     */
     public void unmark() {
         this.isDone = false;
     }
 
+    public abstract LocalDate getDate();
 }
