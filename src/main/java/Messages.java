6480769b685aebfe75e7be3cc2a52cb6b4416d20
seanlaiys <seanlaiyuensheng@gmail.com>
2022-01-27 17:53:19 +0800

    Change Storage class and add TaskList class

--- src/main/java/Messages.java
+++ src/main/java/Messages.java
@@ -2,106 +2,105 @@ import java.util.Scanner;
 
 /**
  *
- * This class contains methods which prints messages Alfred could send
+ * Contains methods which prints messages Alfred could send
  * to the users according to what the users input
  *
  */
 public class Messages {
-    private static final String logo =
+    private static final String LOGO =
             "    __      \n"
-                    + "   /  \\    \n"
-                    + "  / /\\ \\  \n"
-                    + " / /__\\ \\  \n"
-                    + "/_/    \\_\\ \n";
+            + "   /  \\    \n"
+            + "  / /\\ \\  \n"
+            + " / /__\\ \\  \n"
+            + "/_/    \\_\\ \n";
 
-    private static final String greetings =
-            "Greetings, my name is Alfred. \n"
-                    + "How may I assist you?";
+    private static final String GREETING =
+            "Greetings, my name is Alfred. \n" + "How may I assist you?";
 
-    private static final String list = "Here is your task list.";
+    private static final String LIST = "Here is your task list.";
 
-    private static final String blah = "Blah.";
+    private static final String BLAH = "Blah.";
 
-    private static final String bye = "It has been great pleasure to be at your service. Goodbye.";
+    private static final String BYE = "It has been great pleasure to be at your service. Goodbye.";
 
-    private static final String add = "Got it. I've added this task:";
+    private static final String ADD = "Got it. I've added this task:";
 
-    private static final String delete = "Got it. I've removed this task:";
+    private static final String DELETE = "Got it. I've removed this task:";
 
-    private static final String mark = "You have successfully completed the following task:";
+    private static final String MARK = "You have successfully completed the following task:";
 
-    private static final String unmark = "You requested to mark the following task as undone:";
+    private static final String UNMARK = "You requested to mark the following task as undone:";
 
-    private static final String taskSize = "Here is the number of tasks in the list:";
+    private static final String TASK_SIZE = "Here is the number of tasks in the list:";
 
-    private static final String unsure = "☹ OOPS!!! I'm sorry, " +
+    private static final String UNSURE = "☹ OOPS!!! I'm sorry, " +
             "but I don't know what that means :-(";
 
-    private static final String empty = "I can't help you if you don't type anything ☹";
+    private static final String EMPTY = "I can't help you if you don't type anything ☹";
 
-    private static final String noDescription = "Please try again with a description in mind ☹";
+    private static final String NO_DESCRIPTION = "Please try again with a description in mind ☹";
 
     public static void greetUser() {
-        System.out.println(logo);
-        System.out.println(greetings);
+        System.out.println(LOGO);
+        System.out.println(GREETING);
     }
 
-    public static String generateList(Scanner user_input, Storage currentList) {
-        System.out.println(list);
-        System.out.println(currentList.toString());
+    public static String generateList(Scanner user_input, TaskList currentTasks) {
+        System.out.println(LIST);
+        System.out.println(currentTasks.toString());
         String input = user_input.nextLine();
         return input;
     }
 
     public static String sayBlah(Scanner user_input) {
-        System.out.println(blah);
+        System.out.println(BLAH);
         String input = user_input.nextLine();
         return input;
     }
 
-    public static String sayAdd(Scanner user_input, Task newTask, Storage currentStorage) {
-        System.out.println(add);
+    public static String sayAdd(Scanner user_input, Task newTask, TaskList currentTasks) {
+        System.out.println(ADD);
         System.out.println(newTask.toString());
-        System.out.println(taskSize + " " + currentStorage.getSize());
+        System.out.println(TASK_SIZE + " " + currentTasks.getSize());
         String input = user_input.nextLine();
         return input;
     }
 
     public static String sayMark(Scanner user_input, Task task) {
-        System.out.println(mark);
+        System.out.println(MARK);
         System.out.println(task);
         String input = user_input.nextLine();
         return input;
     }
 
     public static String sayUnmark(Scanner user_input, Task task) {
-        System.out.println(unmark);
+        System.out.println(UNMARK);
         System.out.println(task);
         String input = user_input.nextLine();
         return input;
     }
 
-    public static String sayDelete(Scanner user_input, Task task, Storage currentStorage) {
-        System.out.println(delete);
+    public static String sayDelete(Scanner user_input, Task task, TaskList currentTasks) {
+        System.out.println(DELETE);
         System.out.println(task.toString());
-        System.out.println(taskSize + " " + currentStorage.getSize());
+        System.out.println(TASK_SIZE + " " + currentTasks.getSize());
         String input = user_input.nextLine();
         return input;
     }
 
     public static void sayUnsure() {
-        System.out.println(unsure);
+        System.out.println(UNSURE);
     }
 
     public static void sayEmpty() {
-        System.out.println(empty);
+        System.out.println(EMPTY);
     }
 
     public static void sayNoDescription() {
-        System.out.println(noDescription);
+        System.out.println(NO_DESCRIPTION);
     }
 
     public static void sayGoodbye() {
-        System.out.println(bye);
+        System.out.println(BYE);
     }
 }
