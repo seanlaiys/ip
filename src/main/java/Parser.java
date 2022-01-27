050627fa7c7c30ed51c6a08a93f2dc3e17478d8e
seanlaiys <seanlaiyuensheng@gmail.com>
2022-01-28 00:03:35 +0800

    Add LocalDate and LocalTime input

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,8 +1,9 @@
-import java.io.IOException;
-import java.util.Scanner;
-import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.LocalTime;
 /**
  *
  * Serves to take in the input keyed in by the user
@@ -10,6 +11,10 @@ import java.util.Scanner;
  *
  */
 public class Parser {
+    /**
+     * Takes in input from user which contains keywords to commands
+     * to invoke different methods based on the command given
+     */
     public static void readCommand() {
         String input;
         TaskList currentTasks = new TaskList();
@@ -25,7 +30,18 @@ public class Parser {
         input = user_input.nextLine();
         while(!input.equals(Commands.COMMAND_BYE)) {
             if(input.contains(Commands.COMMAND_LIST)) {
-                input = Messages.generateList(user_input, currentTasks);
+                if(input.contains("/on")) {
+                    String[] inputs = input.split("/on ");
+                        try {
+                            LocalDate date = getDate(inputs[1]);
+                            TaskList newTasks = currentTasks.getTasksByDate(date);
+                            input = Messages.generateList(user_input, newTasks);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                } else {
+                    input = Messages.generateList(user_input, currentTasks);
+                }
             } else if (input.contains(Commands.COMMAND_BLAH)) {
                 input = Messages.sayBlah(user_input);
             } else if (input.contains(Commands.COMMAND_UNMARK)){
@@ -69,14 +85,19 @@ public class Parser {
                     Messages.sayNoDescription();
                     input = user_input.nextLine();
                 } else {
-                    String[] inputs = input.split("/");
-                    Task newTask = new Deadline(inputs[0], inputs[1]);
-                    currentTasks.addTasks(newTask);
-                    input = Messages.sayAdd(user_input, newTask, currentTasks);
-                    try {
-                        currentStorage.appendTaskToFile(newTask);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    String[] inputs = input.split("/by ");
+                    if (!isValidTime(inputs[1]) || !isValidDate(inputs[1])) {
+                        Messages.sayInvalidDate();
+                        input = user_input.nextLine();
+                    } else {
+                        try {
+                            Task newTask = new Deadline(inputs[0], getDate(inputs[1]), getTime(inputs[1]));
+                            currentTasks.addTasks(newTask);
+                            input = Messages.sayAdd(user_input, newTask, currentTasks);
+                            currentStorage.appendTaskToFile(newTask);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
             } else if (input.contains(Commands.COMMAND_EVENT)){
@@ -85,14 +106,19 @@ public class Parser {
                     Messages.sayNoDescription();
                     input = user_input.nextLine();
                 } else {
-                    String[] inputs = input.split("/");
-                    Task newTask = new Event(inputs[0], inputs[1]);
-                    currentTasks.addTasks(newTask);
-                    input = Messages.sayAdd(user_input, newTask, currentTasks);
-                    try {
-                        currentStorage.appendTaskToFile(newTask);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    String[] inputs = input.split("/at ");
+                    if (!isValidTime(inputs[1]) || !isValidDate(inputs[1])) {
+                        Messages.sayInvalidDate();
+                        input = user_input.nextLine();
+                    } else {
+                        try {
+                            Task newTask = new Event(inputs[0], getDate(inputs[1]), getTime(inputs[1]));
+                            currentTasks.addTasks(newTask);
+                            input = Messages.sayAdd(user_input, newTask, currentTasks);
+                            currentStorage.appendTaskToFile(newTask);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
             }  else if (input.contains(Commands.COMMAND_DELETE)){
@@ -111,7 +137,7 @@ public class Parser {
                         e.printStackTrace();
                     }
                 }
-            }else if (input.equals("")){
+            } else if (input.equals("")) {
                 Messages.sayEmpty();
                 input = user_input.nextLine();
             } else {
@@ -121,6 +147,13 @@ public class Parser {
         }
         Messages.sayGoodbye();
     }
+
+    /**
+     * Returns the index of the task in the current TaskList
+     * based on the input provided by the user containing a digit
+     *
+     * @return an int index of the task
+     */
     public static int findDigit(String input) {
         char[] inputChars = input.toCharArray();
         String number = "";
@@ -132,5 +165,57 @@ public class Parser {
         int digit = Integer.parseInt(number);
         return digit - 1;
     }
+
+    /**
+     * Returns the index of the task in the current TaskList
+     * based on the input provided by the user containing a digit
+     *
+     * @param input the String input of the LocalDate
+     * @return an int index of the task
+     * @throws IOException if input is invalid
+     */
+    public static LocalDate getDate(String input) throws IOException {
+        String[] dateSplits = input.split("/");
+        String[] yearSplits = dateSplits[2].split(" ");
+        DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("yyyy M d");
+        return LocalDate.parse(yearSplits[0] + " " + dateSplits[1] + " " + dateSplits[0], formatterDate);
+    }
+
+    /**
+     * Returns the index of the task in the current TaskList
+     * based on the input provided by the user containing a digit
+     *
+     * @param input the String input of the LocalDate
+     * @return an int index of the task
+     * @throws IOException if input is invalid
+     */
+    public static LocalTime getTime(String input) throws IOException {
+        String[] dateSplits = input.split("/");
+        String[] yearSplits = dateSplits[2].split(" ");
+        String[] timeSplits = yearSplits[1].split("");
+        return LocalTime.parse(timeSplits[0] + timeSplits[1] + ":" + timeSplits[2] + timeSplits[3]);
+    }
+
+    public static Boolean isValidDate(String input) {
+        String[] dateSplits = input.split("/");
+        if (dateSplits.length < 3) {
+            return false;
+        } else if (dateSplits[2].split(" ")[0].split("").length != 4) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static Boolean isValidTime(String input) {
+        String[] dateSplits = input.split("/");
+        String[] yearSplits = dateSplits[2].split(" ");
+        String[] timeSplits = yearSplits[1].split("");
+        if (timeSplits.length != 4) {
+            return false;
+        }
+        return true;
+    }
+
 
 }
