6ee4d73a1c953cdd7ff078af3326d491e014f966
seanlaiys <seanlaiyuensheng@gmail.com>
2022-01-27 23:57:02 +0800

    Add LocalDate and LocalTime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,15 +1,46 @@
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.time.LocalTime;
+/**
+ * Encapsulates a Deadline which inherits from Task
+ */
 public class Deadline extends Task{
-    private String date;
+    private LocalDate date;
+    private LocalTime time;
     private static final String TYPE = "[D]";
 
-    public Deadline(String task, String date) {
-        super(task);
+    /**
+     * Creates a new Deadline object
+     *
+     * @param deadline a description of the deadline
+     * @param date a date of the deadline
+     * @param time time of the deadline
+     */
+    public Deadline(String deadline, LocalDate date, LocalTime time) {
+        super(deadline);
         this.date = date;
+        this.time = time;
     }
 
+    /**
+     * Getter for the deadline date
+     *
+     * @return a date of the deadline
+     */
+    @Override
+    public LocalDate getDate() {
+        return date;
+    }
+
+    /**
+     * Returns a string representation of the Deadline object
+     *
+     * @return a string describing the status, the deadline and the date
+     */
     @Override
     public String toString() {
         String status = isDone ? DONE : NOTDONE;
-        return TYPE + status + " " + task + "(" + this.date + ")";
+        return TYPE + status + " " + task + "(by: " + date.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+                + " " + time.format(DateTimeFormatter.ofPattern("hh:mm a"))+ ")";
     }
 }
