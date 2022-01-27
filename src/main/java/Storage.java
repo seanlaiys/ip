b1979cdfe5103beec492646af6fe6aaa66c15a52
seanlaiys <seanlaiyuensheng@gmail.com>
2022-01-27 17:53:39 +0800

    Change Storage class

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,48 +1,94 @@
+import javax.print.DocFlavor;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
- * This class serves to store the current tasks in an ArrayList.
+ * Stores the current tasks in an ArrayList.
  *
  */
 public class Storage {
-    private ArrayList<Task> tasks;
+    File taskFile;
+    private boolean isNew;
+
     public Storage() {
-        this.tasks = new ArrayList<Task>();
-    }
-    public void addTasks(Task newTask) {
-        this.tasks.add(newTask);
-    }
-
-    public void removeTasks(int taskNumber) {
-        this.tasks.remove(taskNumber-1);
-    }
-
-    public void markTask(int taskNumber) {
-        Task selected = this.tasks.get(taskNumber-1);
-        selected.mark();
-        this.tasks.set(taskNumber-1, selected);
-    }
-
-    public void unmarkTask(int taskNumber) {
-        Task selected = this.tasks.get(taskNumber-1);
-        selected.unmark();
-        this.tasks.set(taskNumber-1, selected);
-    }
-
-    public Task getTask(int taskNumber) {
-        return this.tasks.get(taskNumber-1);
-    }
-
-    public int getSize() {
-        return this.tasks.size();
-    }
-
-    @Override
-    public String toString() {
-        String list = "";
-        for (int i = 1; i <= this.tasks.size(); i++) {
-            list += i + "."+ this.tasks.get(i-1).toString() + "\n";
+        this.isNew = true;
+        this.taskFile = new File("data/taskFile.txt");
+        try {
+            this.isNew = this.taskFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return list;
+    }
+
+    public boolean isNewFile() {
+        return isNew;
+    }
+
+    private static void appendToFile(String filePath, String textToAppend) throws IOException {
+        FileWriter fw = new FileWriter(filePath, true);
+        fw.write(textToAppend);
+        fw.close();
+    }
+
+    private static void writeToFile(String filePath, String textToAdd) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        fw.write(textToAdd);
+        fw.close();
+    }
+
+    public void writeTasksToFile(TaskList tasks) throws IOException {
+        String fileContent = "";
+        for(int i = 0; i < tasks.getSize(); i++) {
+            fileContent += tasks.getTask(i).toString() + System.lineSeparator();
+        }
+        try {
+            writeToFile(taskFile.getAbsolutePath(), fileContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void appendTaskToFile(Task task) throws IOException {
+        try {
+            appendToFile(taskFile.getAbsolutePath(), task.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public TaskList fileToTaskList() throws FileNotFoundException {
+        Scanner s = new Scanner(taskFile); // create a Scanner using the File as the source
+        String current;
+        TaskList tasks = new TaskList();
+        while (s.hasNext()) {
+            current = s.nextLine();
+            if (current.contains(Commands.COMMAND_TODO)){
+                String[] descriptions = current.split(Commands.COMMAND_TODO);
+                Task newTask = new ToDo(Commands.COMMAND_TODO + descriptions[1]);
+                tasks.addTasks(newTask);
+            } else if (current.contains(Commands.COMMAND_EVENT)){
+                String[] descriptions = current.split(Commands.COMMAND_EVENT);
+                String OPEN_BRAC = "\\(";
+                String CLOSE_BRAC = "\\)";
+                String[] newDescriptions = descriptions[1].split(OPEN_BRAC);
+                String date = newDescriptions[1].split(CLOSE_BRAC)[0];
+                Task newTask = new Event(Commands.COMMAND_EVENT + newDescriptions[0], date);
+                tasks.addTasks(newTask);
+            } else {
+                String[] descriptions = current.split(Commands.COMMAND_DEADLINE);
+                String OPEN_BRAC = "\\(";
+                String CLOSE_BRAC = "\\)";
+                String[] newDescriptions = descriptions[1].split(OPEN_BRAC);
+                String date = newDescriptions[1].split(CLOSE_BRAC)[0];
+                Task newTask = new Deadline(Commands.COMMAND_DEADLINE + newDescriptions[0], date);
+                tasks.addTasks(newTask);
+            }
+        }
+        return tasks;
     }
 }
