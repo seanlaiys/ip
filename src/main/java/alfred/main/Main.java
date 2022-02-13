246852e47f78733cc65bd41df0ee2e2361cad6b5
seanlaiys <seanlaiyuensheng@gmail.com>
2022-02-14 02:01:00 +0800

    Add stage name

--- src/main/java/alfred/main/Main.java
+++ src/main/java/alfred/main/Main.java
@@ -28,6 +28,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setAlfred(alfred);
+            stage.setTitle("Alfred, at your service.");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
